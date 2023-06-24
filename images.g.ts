3729f7da45cd8ede6a1e28872e2ba2735e5bd199
Andy Geers <andy@hubbub.co.uk>
2023-06-24 08:08:53 +0100

    Add proper artwork assets

---
+++ images.g.ts
@@ -0,0 +1,253 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "wall":return img`
+b d d d d d d c b d d d d d d c 
+d b b b b b b c d b b b b b b c 
+d b b b b b b c d b b b b b b c 
+d b b b b b b c d b b b b b b c 
+d b b b b b b c d b b b b b b c 
+d b b b b b b c d b b b b b b c 
+d b b b b b b b d b b b b b b b 
+c c c c c c b a c c c c c c b a 
+b d d d d d d c b d d d d d d c 
+d b b b b b b c d b b b b b b c 
+d b b b b b b c d b b b b b b c 
+d b b b b b b c d b b b b b b c 
+d b b b b b b c d b b b b b b c 
+d b b b b b b c d b b b b b b c 
+d b b b b b b b d b b b b b b b 
+c c c c c c b a b c c c c c c a 
+`;
+            case "Kac}5S_sPY|EDPKP%_(4":
+            case "gravel":return img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 d 7 7 7 7 7 7 7 7 7 
+7 5 7 7 7 7 d 7 7 7 7 7 7 7 7 3 
+7 7 7 7 7 7 7 7 7 7 7 7 5 7 7 d 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 d 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 d 7 7 5 7 7 7 7 7 d 7 7 7 
+7 7 7 3 7 7 7 7 7 7 7 7 d 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 3 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 d 7 7 7 7 7 7 7 
+7 d 7 7 7 5 7 7 7 7 7 7 7 7 7 7 
+7 d 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 d 7 7 5 7 
+7 7 5 7 7 7 7 7 7 7 7 d 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`;
+            case "hz6:s#CodQ":
+            case "road":return img`
+7 7 d b b b b b b b b b b b 7 7 
+7 7 d b b b b b b b a b b b 7 7 
+7 7 d b b b d b b b a b b b 7 7 
+7 7 d b b b b b b b b b b b 7 7 
+7 7 d b b b b b b b b b b b 7 7 
+7 7 d b b b b b b b b b b b 7 7 
+7 7 d b b b b b b b b b b b 7 7 
+7 7 d b b b b b b b b d b b 7 7 
+7 7 d b b b b b b b b b b b 7 7 
+7 7 d b b a b b b b b b b b 7 7 
+7 7 b b b a b b b b b b b d 7 7 
+7 7 b b b b b b b b b b b d 7 7 
+7 7 b b b b b b b b b b b d 7 7 
+7 7 b b d b b b b b b b b d 7 7 
+7 7 b b b b b b b b b b b d 7 7 
+7 7 b b b b b b b b b b b d 7 7 
+`;
+            case "Emaa}":
+            case "player":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . c c c . . . . . . . 
+. . . . . . 5 3 5 . . . . . . . 
+. . . . . . 3 3 3 . . . . . . . 
+. . . . . 2 2 2 2 2 2 2 3 . . . 
+. . . . 2 2 2 2 2 . . . . . . . 
+. . . . 2 . 2 2 2 . . . . . . . 
+. . . . 3 . 2 2 2 . . . . . . . 
+. . . . . . b b b . . . . . . . 
+. . . . . . b b b . . . . . . . 
+. . . . . . b . b . . . . . . . 
+. . . . . b b . b b . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "}lr*M**}hS?1O%]b_QI/":
+            case "tileMap":return img`
+d 7 d d 7 d d 7 d d 7 d d 7 d a 
+d 7 d d 7 d d 7 d d 7 d d 7 d a 
+d 7 d d 7 d d 7 d d 7 d d 7 d a 
+d 7 d d 7 d d 7 d d 7 d d 7 d a 
+d 7 d d 7 d d 7 d d 7 d d 7 d a 
+d 7 d d 7 d d 7 d d 7 d d 7 d a 
+d 7 d d 7 d d 7 d d 7 d d 7 d a 
+d 7 d d 7 d d 7 d d 7 d d 7 d a 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+`;
+            case "image2":
+            case "myImage":return img`
+c c c c c c c d d d d d d d d d 
+c d d d d d c b b b b b b b b b 
+c d b b b b c b b b b b b b b b 
+c d b b b b c b b b b b b b b b 
+c d b b b b c b b b b b b b b b 
+c d b b b b c b b b b b b b b b 
+c c c c c c c b b b b b b b b b 
+d b b b b b b b b b b b b b b b 
+d b b b b b b b b b b b b b b b 
+c c c c c c c b b b b b b b b b 
+c d d d d d c b b b b b b b b b 
+c d b b b b c b b b b b b b b b 
+c d b b b b c b b b b b b b b b 
+c d b b b b c b b b b b b b b b 
+c d b b b b c b b b b b b b b b 
+c c c c c c c b c c c c c c c c 
+`;
+            case "9*Bql`8Kp8EqjvM66P1q":
+            case "enemy1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . f . . . . . . . . 
+. . . . . . f f f . . . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . . f d f d f . . . . . . 
+. . . . . . d d d . . . . . . . 
+. . . . . . a a a . . . . . . . 
+. . . . . . a a a f . . . . . . 
+. . . . . . a a a f . . . . . . 
+. . . . . . a a a f . . . . . . 
+. . . . . . 2 2 2 f . . . . . . 
+. . . . . . 2 . 2 f . . . . . . 
+. . . . . . . . . f . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "yb{Lhbg;":
+            case "enemy2":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . f . . . . . . . . 
+. . . . . . f f f . . . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . . f d f d f . . . . . . 
+. . . . . . d d d . . . . . . . 
+. . . . . . 8 8 8 . . . . . . . 
+. . . . . f 8 8 8 . . . . . . . 
+. . . . f f f 8 8 . . . . . . . 
+. . . . . f 8 8 8 . . . . . . . 
+. . . . . f 4 4 4 . . . . . . . 
+. . . . . f 4 . 4 . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "!N@DEWzvWu8V6|g[6oBV":
+            case "enemy3":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . f . . . . . . . . 
+. . . . . . f f f . . . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . . f d f d f . . . . . . 
+. . . . . . d d d . . . . . . . 
+. . . . . . a a a . . . . . . . 
+. . . . . . a f a f . f . . . . 
+. . . . . . a a f f f . . . . . 
+. . . . . . a a a f . . . . . . 
+. . . . . . 5 5 5 f . . . . . . 
+. . . . . . 5 . 5 . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "uUacm^6sEU[h`)@2#|/f":
+            case "enemy4":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . f . . . . . . . . . 
+. . . . . f f f . . . . . . . . 
+. . . . f f f f f . . . . . . . 
+. . . . f d f d f . . . . . . . 
+. . . . . d d d . . . . . . . . 
+. . . . 2 2 2 2 2 . . . . . . . 
+. . . 2 2 2 2 2 2 f . . . . . . 
+. . . 2 2 2 2 2 f f f . . . . . 
+. . . . 2 2 2 2 2 f . . . . . . 
+. . . . 8 8 8 8 8 f . . . . . . 
+. . . . 8 . . . 8 f . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "qx-^jy@Q:j`_mN2HR;f=":
+            case "enemy5":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . f . . . . . . . . 
+. . . . . . f f f . . . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . . f d f d f . . . . . . 
+. . . . . . d d d . . . . . . . 
+. . . . . . 8 8 8 . . . . . . . 
+. . . . . f 8 f 8 . . . . . . . 
+. . . . . . f 8 8 f . . . . . . 
+. . . . . f 8 f 8 f . . . . . . 
+. . . . . . 2 2 2 f . . . . . . 
+. . . . . . 2 . 2 . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "eB`kHM4?B4y-Y*SWfV8y":
+            case "boss":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. f . . . . . f f f f f . . . . 
+. f f . . . f f f f f f f . . . 
+. . . f . f f . f f f . f f . f 
+. . . . f 5 . 5 5 f 5 . 5 . f f 
+. . . . 5 . 8 5 5 5 5 5 8 f f . 
+. . . . . 5 8 8 5 5 5 8 8 3 . . 
+. . . . . 5 8 8 8 8 8 8 8 3 . . 
+. . . . . 8 8 8 8 8 8 8 8 8 . . 
+. . . . f 5 8 8 8 8 8 8 8 . . . 
+. . f f . . 2 2 8 8 8 2 2 f f . 
+. f f . . 2 2 2 2 2 2 2 2 2 f f 
+. . . . . . 2 2 . . . 2 2 . . f 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
